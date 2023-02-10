ff4e3adc97df685c0e23115090f512c9c405965b
Leo <leojunebedeo@gmail.com>
2023-02-10 18:09:34 +0800

    Add meta data

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
-<script setup></script>
+<script setup>
+useServerSeoMeta({
+  title: 'DevHQ',
+  ogTitle: 'DevHQ',
+  description: 'DevHQ provides solutions for web development tasks.',
+  ogDescription: 'DevHQ provides solutions for web development tasks.',
+  ogImage: 'https://media.istockphoto.com/id/1219854221/photo/web-design-development-and-coding-concept.jpg?s=612x612&w=0&k=20&c=FqSjdRh6subL_UMSpuXxeRpvkt1krxUiyCQZ1ws7rOk=',
+  twitterCard: 'summary_large_image',
+})
+</script>
 
 <template>
     <div>
