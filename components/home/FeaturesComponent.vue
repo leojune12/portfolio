4cd772dd2c97963de07494378950739dfcf7f321
Leo <leojunebedeo@gmail.com>
2023-02-13 14:58:45 +0800

    Update margin left

--- components/home/FeaturesComponent.vue
+++ components/home/FeaturesComponent.vue
@@ -8,7 +8,7 @@
             <div class="space-y-8 sm:grid sm:grid-cols-3 sm:gap-12 sm:space-y-0">
                 <div>
                     <div class="flex md:flex-col flex-row md:gap-x-0 gap-x-2 items-center md:items-start md:mb-0 mb-4">
-                        <div class="md:mb-3 mb-0 w-16 h-16 lg:h-28 lg:w-28 -ml-3.5">
+                        <div class="md:mb-3 mb-0 w-16 h-16 lg:h-28 lg:w-28 -ml-2 md:-ml-3.5">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-orange-600">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
                             </svg>
@@ -119,7 +119,7 @@
                 </div>
                 <div>
                     <div class="flex md:flex-col flex-row md:gap-x-0 gap-x-2 items-center md:items-start md:mb-0 mb-4">
-                        <div class="md:mb-3 mb-0 w-16 h-16 lg:h-28 lg:w-28 -ml-3.5">
+                        <div class="md:mb-3 mb-0 w-16 h-16 lg:h-28 lg:w-28 -ml-2 md:-ml-3.5">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-blue-600">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
                             </svg>
@@ -164,7 +164,7 @@
                 </div>
                 <div>
                     <div class="flex md:flex-col flex-row md:gap-x-0 gap-x-2 items-center md:items-start md:mb-0 mb-4">
-                        <div class="md:mb-3 mb-0 w-16 h-16 lg:h-28 lg:w-28 -ml-3.5 p-2">
+                        <div class="md:mb-3 mb-0 w-16 h-16 lg:h-28 lg:w-28 -ml-2 md:-ml-3.5 p-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-green-600">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
                             </svg>
